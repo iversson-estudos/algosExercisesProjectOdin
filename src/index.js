3ddf9542d32eea80b5c006dc6feb647aa69aae63
iversson-estudos <i versson.estudos@gmail.com>
2025-01-09 01:40:53 -0300

    done and set

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+function fiboRecursive(sequence, sum = 0, prev = 1) {
+  if (sequence == 0) {
+    console.log(sum);
+    return sum;
+  } else {
+    console.log(sum);
+    return fiboRecursive(sequence - 1, sum + prev, sum);
+  }
+}
