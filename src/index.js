85ddb63f155f7c8e53ee2ebc8c518c584f68d344
iversson-estudos <i versson.estudos@gmail.com>
2025-01-10 10:35:08 -0300

    trying to come up with an idea for this

--- src/index.js
+++ src/index.js
@@ -13,4 +13,19 @@ function fiboRecursive(sequence, sum = 0, prev = 1) {
   }, 1000);
 }
 
-fiboRecursive(300);
+/**MERGE SORT */
+function mergeSortRecursive(array) {
+  /*FIRST WE DIVIDE THE ARRAY INTO SMALLERS LEFT AND RIGHT */
+  if (array.length > 2) {
+    const arrayleft = array.slice(0, Math.floor(array.length / 2));
+    const arrayRight = array.slice(Math.floor(array.length / 2));
+
+    console.log(array);
+
+    mergeSortRecursive(arrayleft);
+    mergeSortRecursive(arrayRight);
+  } else if (array.length == 1) {
+    return array;
+  } else {
+  }
+}
